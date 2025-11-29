7e1413dfcb98f5da0e0f0e82b61da9a5aab95c0b
plario199 <laetitiabougchiche@gmail.com>
2025-11-29 11:30:33 -0500

    Add setters and getters for patient attributes

--- Patient.h
+++ Patient.h
@@ -25,4 +25,33 @@ long assignedDoctorId,
 const string &dateOfBirth,
 const string &bloodType,
 const string &diagnosis,
-const string &dateO
+const string &dateOfAdmission, 
+const string &dateOfDischarge);
+
+//setters
+void SetFirstName(const string &value);
+void SetLastName( const string &value);
+void SetPatientId(long value);
+void SetAssignedDoctorId(long value);
+void SetDateOfBirth(const string &value);
+void setBloodType( const string &value);
+void SetDiagnosis( const string &value);
+void SetDateOfAdmission(const string &value);
+void SetDateOfDischarge(const string &value);
+
+//getters
+string GetFirstName() const;
+string GetLastName() const;
+long GetPatientId() const;
+long GetAssignedDoctorId() const;
+string GetDateOfBirth() const;
+string GetBloodType() const;
+string GetDiagnosis() const;
+string GetDateOfAdmission() const;
+string GetDateOfDischarge() const;
+
+bool IsDischarged() const; // true if dicharge date !="-1"
+string PatientStatus() const; // "Critical", "MOderate", " Stable"
+void PrintPatientInfo(0 const; // print all fields 
+};
+endif 
